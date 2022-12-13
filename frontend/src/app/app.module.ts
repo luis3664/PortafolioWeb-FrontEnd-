00cdb80f389788f245b682b8a3b5bb4d464e8be3
Luis Lopez <luis.lopez3664mail.com>
2022-12-13 16:24:01 -0300

    Adicion de lo que faltaba del maquetado y correcciones en cards

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ArticleComponent } from './components/article/article.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule} from '@angular/common/http';
-import { CardsPresentationComponent } from './components/cards-presentation/cards-presentation.component'
+import { CardsPresentationComponent } from './components/cards-presentation/cards-presentation.component';
+import { WorksComponent } from './components/works/works.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArticleComponent,
     FooterComponent,
-    CardsPresentationComponent
+    CardsPresentationComponent,
+    WorksComponent,
+    CoursesComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
