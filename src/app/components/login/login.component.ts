80095aa7e9fc0b091f373fc8ab924a94f486d081
Luis Lopez <luis.lopez3664mail.com>
2023-01-24 16:47:46 -0300

    Termine la validacion de usuarios y la implementacionde cache para el token con un timer

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -14,12 +15,22 @@ export class LoginComponent {
   // Inject
   private _authService = inject(AuthService);
   private _formBuilder = inject(FormBuilder);
+  private cookies = inject(CookieService);
 
   constructor(){
-    this.singPorfolio = this._formBuilder.group({
-      email: new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl ('',[Validators.required, Validators.minLength(8)])
-    })
+    if(this.remember()){
+      this.singPorfolio = this._formBuilder.group({
+        email: new FormControl(this.cookies.get("rememberMail"),[Validators.required, Validators.email]),
+        password: new FormControl ('',[Validators.required, Validators.minLength(8)]),
+        rememberMail: new FormControl (true)
+      })
+    }else{
+      this.singPorfolio = this._formBuilder.group({
+        email: new FormControl("",[Validators.required, Validators.email]),
+        password: new FormControl ('',[Validators.required, Validators.minLength(8)]),
+        rememberMail: new FormControl (false)
+      })
+    }
   }
 
   public onSend(event: Event) {
@@ -28,11 +39,22 @@ export class LoginComponent {
     if (this.singPorfolio.valid){
       let email = this.singPorfolio.getRawValue().email;
       let password = this.singPorfolio.getRawValue().password;
-      this._authService.login(email, password)
+
+      if(this.singPorfolio.getRawValue().rememberMail){
+        this.cookies.set("rememberMail", this.singPorfolio.getRawValue().email);
+      }else{
+        this.cookies.set("rememberMail", "");
+      };
+
+      this._authService.login(email, password);
     }else{
       this.singPorfolio.markAllAsTouched();
     }
 
+  }
+
+  private remember(): boolean{
+    return (this.cookies.get("rememberMail") != "");
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -8,39 +9,51 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class AuthService {
 
-
+  // Inject
   private _firebase = inject(AngularFireAuth);
   private router = inject(Router);
+  private cookies = inject(CookieService);
 
   constructor() {
   }
 
   public login(email:string, password: string) {
 
+    let timer: Date = new Date();
+    timer.setMinutes( timer.getMinutes() + 15);
+    
     this._firebase.signInWithEmailAndPassword(email, password).then(result =>{
+      alert("Valid Entry");
       result.user?.getIdToken().then(token =>{
-        localStorage.setItem("auth_token", token);
+        // localStorage.setItem("auth_token", token);
+        this.cookies.set("auth_token", token, timer);
         this.router.navigate(['/']);
       })
-    })
+    }).catch(err =>{
+      alert("Invalid Email and Password combination.")
+    });
 
   }
 
   public getIdToken(): string{
-    return localStorage.getItem("auth_token") as string;
+    return this.cookies.get("auth_token");
+    // return localStorage.getItem("auth_token") as string;
   }
 
   public logout() {
     this._firebase.signOut().then(() =>{
-      localStorage.removeItem("auth_token");
+      this.cookies.set("auth_token", "");
+      // localStorage.removeItem("auth_token");
       this.router.navigate(['/index']);
       window.location.reload();
     });
   }
 
   public get logState(): boolean {
-    let token: string = localStorage.getItem("auth_token") as string;
-    return (token != undefined);
+    // let token: string = localStorage.getItem("auth_token") as string;
+    // return (token != undefined);
+    let token: string = this.cookies.get("auth_token");
+    return (token != "");
   }
 
 }
